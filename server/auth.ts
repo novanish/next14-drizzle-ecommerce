fcfb38e93e256c8ddd599d20d858eb010ee30005
novanish <anishchhetri196@gmail.com>
2024-11-25 19:28:27 +0545

    feat(auth): add Google authentication provider

--- server/auth.ts
+++ server/auth.ts
@@ -1,8 +1,16 @@
 import NextAuth from "next-auth";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from ".";
+import GoogleProvider from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
-  providers: [],
+  secret: process.env.AUTH_SECRET,
+  session: { strategy: "jwt" },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
 });
