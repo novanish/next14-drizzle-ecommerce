573fa6252c68417e377af192e90475f702a7a732
novanish <anishchhetri196@gmail.com>
2024-11-29 20:10:38 +0545

    feat: enhance database initialization with schema and logging

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
+import * as schema from "@/server/schema";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is required");
@@ -8,4 +9,8 @@ if (!process.env.DATABASE_URL) {
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
-export const db = drizzle({ client: pool });
+export const db = drizzle({
+  client: pool,
+  schema,
+  logger: process.env.NODE_ENV === "development",
+});
