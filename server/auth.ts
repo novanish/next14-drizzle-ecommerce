88cfd32d86a33c61d56324ff517969ead2a4e5c5
novanish <anishchhetri196@gmail.com>
2024-12-01 20:13:53 +0545

    feat: add credentials provider for NextAuth

--- server/auth.ts
+++ server/auth.ts
@@ -2,6 +2,10 @@ import NextAuth from "next-auth";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from ".";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { loginSchema } from "@/types/login-schema";
+import { eq } from "drizzle-orm";
+import { users } from "./schema";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
@@ -11,6 +15,22 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+
+    CredentialsProvider({
+      async authorize(credentials) {
+        const validatedFields = loginSchema.safeParse(credentials);
+        if (!validatedFields.success) {
+          return null;
+        }
+
+        const { email } = validatedFields.data;
+        const user = await db.query.users.findFirst({
+          columns: { password: false },
+          where: eq(users.email, email),
+        });
+        return user ?? null;
+      },
     }),
   ],
 });
