838a19f28f5863dc18f7ade361636c14841bc366
novanish <anishchhetri196@gmail.com>
2024-12-04 20:04:08 +0545

    feat(auth): enhance authentication with JWT callbacks and user role support

--- server/auth.ts
+++ server/auth.ts
@@ -1,16 +1,56 @@
-import NextAuth from "next-auth";
-import { DrizzleAdapter } from "@auth/drizzle-adapter";
-import { db } from ".";
-import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
 import { loginSchema } from "@/types/login-schema";
-import { eq } from "drizzle-orm";
-import { users } from "./schema";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { eq, sql } from "drizzle-orm";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import { db } from ".";
+import { accounts, users } from "./schema";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
   secret: process.env.AUTH_SECRET,
   session: { strategy: "jwt" },
+  callbacks: {
+    async session({ session, token }) {
+      if (!token.sub) return session;
+      Object.assign(session.user, token);
+      session.user.id = token.sub;
+
+      return session;
+    },
+
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const [
+        user,
+        {
+          rows: [{ isOAuth }],
+        },
+      ] = await Promise.all([
+        db.query.users.findFirst({
+          columns: { password: false },
+          where: eq(users.id, token.sub),
+        }),
+
+        db.execute<{ isOAuth: boolean }>(
+          sql`select exists (select 1 from ${accounts} where ${accounts.userId} = ${token.sub}) as "isOAuth"`
+        ),
+      ]);
+
+      if (!user) return token;
+
+      token.isOAuth = isOAuth;
+      token.role = user.role;
+      token.twoFactorEnabled = user.twoFactorEnabled;
+      token.name = user.name;
+      token.image = user.image;
+
+      return token;
+    },
+  },
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
