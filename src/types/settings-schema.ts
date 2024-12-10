5b861868ffc2e0ac323c56e7c7df37914b98130b
novanish <anishchhetri196@gmail.com>
2024-12-10 18:20:21 +0545

    refactor: rename `isTwoFactorEnabled` to `twoFactorEnabled` in settings schema

--- src/types/settings-schema.ts
+++ src/types/settings-schema.ts
@@ -3,7 +3,7 @@ import * as z from "zod";
 export const settingSchema = z.object({
   name: z.string().optional(),
   image: z.string().optional(),
-  isTwoFactorEnabled: z.boolean().optional(),
+  twoFactorEnabled: z.boolean().optional(),
 });
 
 export type SettingSchema = z.infer<typeof settingSchema>;
