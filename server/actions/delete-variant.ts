3dc1de0f304a7878df16c957aa38af12fcb2cc98
novanish <anishchhetri196@gmail.com>
2024-12-12 19:17:39 +0545

    feat: add deleteVariant server action

---
+++ server/actions/delete-variant.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import { eq } from "drizzle-orm";
+import { createSafeActionClient } from "next-safe-action";
+import { revalidatePath } from "next/cache";
+import { z } from "zod";
+import { db } from "..";
+import { auth } from "../auth";
+import { productVariants } from "../schema";
+
+const actionClient = createSafeActionClient();
+
+export const deleteVariant = actionClient
+  .schema(
+    z.object({
+      id: z.number(),
+    })
+  )
+  .action(async ({ parsedInput }) => {
+    const session = await auth();
+    const isAdmin = session?.user.role === "ADMIN";
+    if (!isAdmin) {
+      return { error: "Unauthorized" };
+    }
+
+    await db
+      .delete(productVariants)
+      .where(eq(productVariants.id, parsedInput.id));
+    revalidatePath("/dashboard/products");
+  });
