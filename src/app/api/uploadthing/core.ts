f2fc4402a55da8d8893b0e4b732573e387aeaba9
novanish <anishchhetri196@gmail.com>
2024-12-13 17:56:43 +0545

    feat(api): update uploadthing core configuration for file uploads

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,26 +1,21 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { UploadThingError } from "uploadthing/server";
 
 const f = createUploadthing();
 
-const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
-
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
   avatarUploader: f({
     image: {
       maxFileSize: "2MB",
       maxFileCount: 1,
     },
-  }).onUploadComplete(async ({ metadata, file }) => {
-    // This code RUNS ON YOUR SERVER after upload
-    console.log("Upload complete for userId:", metadata.userId);
+  }).onUploadComplete(async () => {}),
 
-    console.log("file url", file.url);
-
-    // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-    return { uploadedBy: "server" };
-  }),
+  variantImage: f({
+    image: {
+      maxFileSize: "4MB",
+      maxFileCount: 7,
+    },
+  }).onUploadComplete(async () => {}),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
