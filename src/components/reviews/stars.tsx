b322c6c950ea62b49287543cb17b88e2a97d33fa
novanish <anishchhetri196@gmail.com>
2024-12-22 20:02:32 +0545

    feat: add reusable Stars component for displaying ratings

---
+++ src/components/reviews/stars.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Star } from "lucide-react";
+
+interface Props {
+  rating: number;
+  totalReviews?: number;
+  size?: number;
+}
+
+export function Stars({ rating, totalReviews, size = 14 }: Props) {
+  return (
+    <div className="flex items-center">
+      {[1, 2, 3, 4, 5].map((star) => (
+        <Star
+          size={size}
+          key={star}
+          className={cn(
+            "text-primary bg-transparent transition-all duration-300 ease-in-out",
+            rating >= star ? "fill-primary" : "fill-transparent"
+          )}
+        ></Star>
+      ))}
+      {totalReviews ? (
+        <span className="text-secondary-foreground font-bold text-sm ml-2">
+          {totalReviews} reviews
+        </span>
+      ) : null}
+    </div>
+  );
+}
