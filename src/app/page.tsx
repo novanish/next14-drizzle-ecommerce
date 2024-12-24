e02033aebfc26b4c289a987ee92e758277aebc19
novanish <anishchhetri196@gmail.com>
2024-12-24 18:16:29 +0545

    feat: display product variants on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import { Products } from "@/components/products";
+import { db } from "@/server/index";
+
 export default async function Home() {
-  return <div></div>;
+  const variants = await db.query.productVariants.findMany({
+    with: {
+      images: { columns: { url: true }, limit: 1 },
+      tags: { columns: { tag: true }, limit: 1 },
+      product: { columns: { title: true, price: true } },
+    },
+    limit: 20,
+  });
+
+  return (
+    <>
+      <Products variants={variants} />
+    </>
+  );
 }
