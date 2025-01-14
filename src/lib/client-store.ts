beea94917a915f027a2466c58ba6a3a4e6ef4f44
novanish <anishchhetri196@gmail.com>
2025-01-14 18:35:31 +0545

    feat(client-store): enhance cart store functionality and state management

--- src/lib/client-store.ts
+++ src/lib/client-store.ts
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
 interface Variant {
-  id: string;
+  id: number;
   quantity: number;
 }
 
@@ -14,17 +14,43 @@ interface CartItem {
   variant: Variant;
 }
 
+type CartProgressState =
+  (typeof CART_PROGRESS_STATE)[keyof typeof CART_PROGRESS_STATE];
+
 interface CartState {
   cart: Array<CartItem>;
   addToCart: (i: CartItem) => void;
-  removeItem: (id: string) => void;
-  changeQuantityTo: (id: string, to: ((q: number) => number) | number) => void;
+  removeItem: (id: number) => void;
+  changeQuantityTo: (id: number, to: ((q: number) => number) | number) => void;
+  progress: CartProgressState;
+  changeProgressTo: (to: CartProgressState) => void;
+  clearCart: () => void;
+  isCartOpen: boolean;
+
+  toggleCartOpen: (v?: boolean) => void;
 }
+
+export const CART_PROGRESS_STATE = {
+  CART_PAGE: "cart-page",
+  PAYMENT_PAGE: "payment-page",
+  CONFIRMATION_PAGE: "confirmation-page",
+} as const;
 
 export const useCartStore = create(
   persist<CartState>(
     (set, get) => ({
       cart: [],
+      progress: CART_PROGRESS_STATE.CART_PAGE,
+      isCartOpen: false,
+
+      toggleCartOpen(v) {
+        set(() => ({ isCartOpen: v ?? !get().isCartOpen }));
+      },
+
+      clearCart: () => {
+        set(() => ({ cart: [] }));
+      },
+
       addToCart: (item) => {
         set(() => {
           let exists = false;
@@ -71,6 +97,10 @@ export const useCartStore = create(
           const cart = get().cart.filter((item) => item.variant.id !== id);
           return { cart };
         });
+      },
+
+      changeProgressTo: (to) => {
+        set(() => ({ progress: to }));
       },
     }),
     { name: "cart-storage", storage: createJSONStorage(() => localStorage) }
