e43a8227e7137854d45afe751253b9086d1d78d1
novanish <anishchhetri196@gmail.com>
2025-01-22 20:52:37 +0545

    refactor(products): simplify ProductTags component with dynamic tag rendering

--- src/components/products/product-tags.tsx
+++ src/components/products/product-tags.tsx
@@ -4,59 +4,47 @@ import { cn } from "@/lib/utils";
 import { Badge } from "../ui/badge";
 import { useRouter, useSearchParams } from "next/navigation";
 
-export default function ProductTags() {
+const tags = [
+  { name: "All", color: "bg-black-50 hover:bg-black/75 hover:opacity-100" },
+  { name: "Blue", color: "bg-blue-500 hover:bg-blue-600 hover:opacity-100" },
+  {
+    name: "Green",
+    color: "bg-green-500 hover:bg-green-600 hover:opacity-100",
+  },
+  {
+    name: "Purple",
+    color: "bg-purple-500 hover:bg-purple-600 hover:opacity-100",
+  },
+];
+
+export function ProductTags() {
   const router = useRouter();
   const params = useSearchParams();
-  const tag = params.get("tag");
+  const selectedTag = params.get("tag") || "all";
 
   const setFilter = (tag: string) => {
-    if (tag) {
-      router.push(`?tag=${tag}`);
-    }
-
-    if (!tag) {
-      router.push("/");
-    }
+    router.push(`?tag=${tag}`);
   };
 
   return (
     <div className="my-4 flex gap-4 items-center justify-center">
-      <Badge
-        onClick={() => setFilter("")}
-        className={cn(
-          "cursor-pointer bg-black hover:bg-black/75 hover:opacity-100",
-          !tag ? "opacity-100" : "opacity-50"
-        )}
-      >
-        All
-      </Badge>
-      <Badge
-        onClick={() => setFilter("blue")}
-        className={cn(
-          "cursor-pointer bg-blue-500 hover:bg-blue-600 hover:opacity-100",
-          tag === "blue" && tag ? "opacity-100" : "opacity-50"
-        )}
-      >
-        Blue
-      </Badge>
-      <Badge
-        onClick={() => setFilter("green")}
-        className={cn(
-          "cursor-pointer bg-green-500 hover:bg-green-600 hover:opacity-100",
-          tag === "green" && tag ? "opacity-100" : "opacity-50"
-        )}
-      >
-        Green
-      </Badge>
-      <Badge
-        onClick={() => setFilter("purple")}
-        className={cn(
-          "cursor-pointer bg-purple-500 hover:bg-purple-600 hover:opacity-100",
-          tag === "purple" && tag ? "opacity-100" : "opacity-50"
-        )}
-      >
-        Purple
-      </Badge>
+      {tags.map((tag) => {
+        const tagName = tag.name.toLowerCase();
+
+        return (
+          <Badge
+            key={tag.name}
+            onClick={() => setFilter(tagName)}
+            className={cn(
+              "cursor-pointer",
+              tag.color,
+              tagName === selectedTag && tag ? "opacity-100" : "opacity-50"
+            )}
+          >
+            {tag.name}
+          </Badge>
+        );
+      })}
     </div>
   );
 }
